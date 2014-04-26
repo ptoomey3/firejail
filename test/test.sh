a817b7d2f5c1ea53535740d007efad54644e97ff
netblue30 <netblue30@a7c41c1e-9cec-41a6-8168-6dbd64aeb3cc>
2014-04-26 17:35:48 +0000

    0.9.2 testing

--- test/test.sh
+++ test/test.sh
@@ -33,6 +33,13 @@ echo "TESTING: profile tmpfs"
 echo "TESTING: private"
 ./private.exp
 
+grep "openSUSE" /etc/os-release
+if [ "$?" -eq 0 ];
+then
+	echo "TESTING: overlayfs"
+	./fs_overlay.exp
+fi
+
 echo "TESTING: local network"
 ./net_local.exp
 

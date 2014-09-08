1280c1a86116ff67f7537da0b505ab37cd33fc69
netblue30 <netblue30@a7c41c1e-9cec-41a6-8168-6dbd64aeb3cc>
2014-09-08 16:59:10 +0000

    fixes

--- platform/rpm/mkrpm.sh
+++ platform/rpm/mkrpm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-rm -fr rpmbuild
+VERSION="0.9.12"
+rm -fr ~/rpmbuild
+rm -f firejail-$VERSION-1.x86_64.rpm
 
 mkdir -p ~/rpmbuild/{RPMS,SRPMS,BUILD,SOURCES,SPECS,tmp}
 cat <<EOF >~/.rpmmacros
@@ -9,32 +11,33 @@ EOF
 
 cd ~/rpmbuild
 
-mkdir -p firejail-0.9.6/usr/bin
-install -m 755 /usr/bin/firejail firejail-0.9.6/usr/bin/.
-install -m 755 /usr/bin/firemon firejail-0.9.6/usr/bin/.
+mkdir -p firejail-$VERSION/usr/bin
+install -m 755 /usr/bin/firejail firejail-$VERSION/usr/bin/.
+install -m 755 /usr/bin/firemon firejail-$VERSION/usr/bin/.
 
-mkdir -p firejail-0.9.6/usr/share/man/man1
-install -m 644 /usr/share/man/man1/firejail.1.gz firejail-0.9.6/usr/share/man/man1/.
-install -m 644 /usr/share/man/man1/firemon.1.gz firejail-0.9.6/usr/share/man/man1/.
+mkdir -p firejail-$VERSION/usr/share/man/man1
+install -m 644 /usr/share/man/man1/firejail.1.gz firejail-$VERSION/usr/share/man/man1/.
+install -m 644 /usr/share/man/man1/firemon.1.gz firejail-$VERSION/usr/share/man/man1/.
 
-mkdir -p firejail-0.9.6/usr/share/doc/packages/firejail
-install -m 644 /usr/share/doc/firejail/COPYING firejail-0.9.6/usr/share/doc/packages/firejail/.
-install -m 644 /usr/share/doc/firejail/README firejail-0.9.6/usr/share/doc/packages/firejail/.
-install -m 644 /usr/share/doc/firejail/RELNOTES firejail-0.9.6/usr/share/doc/packages/firejail/.
+mkdir -p firejail-$VERSION/usr/share/doc/packages/firejail
+install -m 644 /usr/share/doc/firejail/COPYING firejail-$VERSION/usr/share/doc/packages/firejail/.
+install -m 644 /usr/share/doc/firejail/README firejail-$VERSION/usr/share/doc/packages/firejail/.
+install -m 644 /usr/share/doc/firejail/RELNOTES firejail-$VERSUIB/usr/share/doc/packages/firejail/.
 
-mkdir -p firejail-0.9.6/etc/firejail
-install -m 644 /etc/firejail/firefox.profile firejail-0.9.6/etc/firejail/firefox.profile
-install -m 644 /etc/firejail/evince.profile firejail-0.9.6/etc/firejail/evince.profile
-install -m 644 /etc/firejail/midori.profile firejail-0.9.6/etc/firejail/midori.profile
+mkdir -p firejail-$VERSION/etc/firejail
+install -m 644 /etc/firejail/firefox.profile firejail-$VERSION/etc/firejail/firefox.profile
+install -m 644 /etc/firejail/evince.profile firejail-$VERSION/etc/firejail/evince.profile
+install -m 644 /etc/firejail/midori.profile firejail-$VERSION/etc/firejail/midori.profile
+install -m 644 /etc/chromium-browser.profile firejail-$VERSION/etc/firejail/.
+install -m 644 /etc/chromium.profile firejail-$VERSION/etc/firejail/.
+install -m 644 /etc/firejail/login.users firejail-$VERSION/etc/firejail/login.users
 
-install -m 644 /etc/firejail/login.users firejail-0.9.6/etc/firejail/login.users
+mkdir -p firejail-$VERSION/usr/share/bash-completion/completions
+install -m 644 /usr/share/bash-completion/completions/firejail  firejail-$VERSION/usr/share/bash-completion/completions/.
 
-mkdir -p firejail-0.9.6//usr/share/bash-completion/completions
-install -m 644 /usr/share/bash-completion/completions/firejail  firejail-0.9.6//usr/share/bash-completion/completions/.
+tar -czvf firejail-$VERSION.tar.gz firejail-$VERSION
 
-tar -czvf firejail-0.9.6.tar.gz firejail-0.9.6
-
-cp firejail-0.9.6.tar.gz SOURCES/.
+cp firejail-$VERSION.tar.gz SOURCES/.
 
 cat <<EOF > SPECS/firejail.spec
 %define        __spec_install_post %{nil}
@@ -43,7 +46,7 @@ cat <<EOF > SPECS/firejail.spec
 
 Summary: Linux namepaces sandbox program
 Name: firejail
-Version: 0.9.6
+Version: $VERSION
 Release: 1
 License: GPL+
 Group: Development/Tools
@@ -78,6 +81,8 @@ rm -rf %{buildroot}
 %config(noreplace) %{_sysconfdir}/%{name}/firefox.profile
 %config(noreplace) %{_sysconfdir}/%{name}/midori.profile
 %config(noreplace) %{_sysconfdir}/%{name}/evince.profile
+%config(noreplace) %{_sysconfdir}/%{name}/chromium.profile
+%config(noreplace) %{_sysconfdir}/%{name}/chromium-browser.profile
 %config(noreplace) %{_sysconfdir}/%{name}/login.users
 %{_bindir}/*
 %{_docdir}/*
@@ -126,7 +131,8 @@ chmod u+s /usr/bin/firejail
 EOF
 
 rpmbuild -ba SPECS/firejail.spec
-rpm -qpl RPMS/x86_64/firejail-0.9.6-1.x86_64.rpm
+rpm -qpl RPMS/x86_64/firejail-$VERSION-1.x86_64.rpm
 cd ..
-rm -f firejail-0.9.6-1.x86_64.rpm
-cp rpmbuild/RPMS/x86_64/firejail-0.9.6-1.x86_64.rpm .
+rm -f firejail-$VERSION-1.x86_64.rpm
+cp rpmbuild/RPMS/x86_64/firejail-$VERSION-1.x86_64.rpm .
+

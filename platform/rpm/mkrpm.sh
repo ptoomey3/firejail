5fd1dd086ccb45fbfaeb2ef2bf0236bb5623ecba
netblue30 <netblue30@a7c41c1e-9cec-41a6-8168-6dbd64aeb3cc>
2014-10-08 17:02:12 +0000

    0.9.14 testing

--- platform/rpm/mkrpm.sh
+++ platform/rpm/mkrpm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="0.9.12.2"
+VERSION="0.9.14"
 rm -fr ~/rpmbuild
 rm -f firejail-$VERSION-1.x86_64.rpm
 
@@ -10,10 +10,14 @@ cat <<EOF >~/.rpmmacros
 EOF
 
 cd ~/rpmbuild
+echo "building directory tree"
 
 mkdir -p firejail-$VERSION/usr/bin
 install -m 755 /usr/bin/firejail firejail-$VERSION/usr/bin/.
 install -m 755 /usr/bin/firemon firejail-$VERSION/usr/bin/.
+
+mkdir -p  firejail-$VERSION/usr/lib/firejail
+install -m 644 /usr/lib/firejail/libtrace.so  firejail-$VERSION/usr/lib/firejail/.
 
 mkdir -p firejail-$VERSION/usr/share/man/man1
 install -m 644 /usr/share/man/man1/firejail.1.gz firejail-$VERSION/usr/share/man/man1/.
@@ -22,14 +26,12 @@ install -m 644 /usr/share/man/man1/firemon.1.gz firejail-$VERSION/usr/share/man/
 mkdir -p firejail-$VERSION/usr/share/doc/packages/firejail
 install -m 644 /usr/share/doc/firejail/COPYING firejail-$VERSION/usr/share/doc/packages/firejail/.
 install -m 644 /usr/share/doc/firejail/README firejail-$VERSION/usr/share/doc/packages/firejail/.
-install -m 644 /usr/share/doc/firejail/RELNOTES firejail-$VERSUIB/usr/share/doc/packages/firejail/.
+install -m 644 /usr/share/doc/firejail/RELNOTES firejail-$VERSION/usr/share/doc/packages/firejail/.
 
 mkdir -p firejail-$VERSION/etc/firejail
 install -m 644 /etc/firejail/firefox.profile firejail-$VERSION/etc/firejail/firefox.profile
 install -m 644 /etc/firejail/evince.profile firejail-$VERSION/etc/firejail/evince.profile
 install -m 644 /etc/firejail/midori.profile firejail-$VERSION/etc/firejail/midori.profile
-install -m 644 /etc/chromium-browser.profile firejail-$VERSION/etc/firejail/.
-install -m 644 /etc/chromium.profile firejail-$VERSION/etc/firejail/.
 install -m 644 /etc/firejail/login.users firejail-$VERSION/etc/firejail/login.users
 install -m 644 /etc/firejail/chromium.profile firejail-$VERSION/etc/firejail/chromium.profile
 install -m 644 /etc/firejail/chromium-browser.profile firejail-$VERSION/etc/firejail/chromium-browser.profile
@@ -38,10 +40,12 @@ install -m 644 /etc/firejail/chromium-browser.profile firejail-$VERSION/etc/fire
 mkdir -p firejail-$VERSION/usr/share/bash-completion/completions
 install -m 644 /usr/share/bash-completion/completions/firejail  firejail-$VERSION/usr/share/bash-completion/completions/.
 
+echo "building tar.gz archive"
 tar -czvf firejail-$VERSION.tar.gz firejail-$VERSION
 
 cp firejail-$VERSION.tar.gz SOURCES/.
 
+echo "building config spec"
 cat <<EOF > SPECS/firejail.spec
 %define        __spec_install_post %{nil}
 %define          debug_package %{nil}
@@ -87,15 +91,34 @@ rm -rf %{buildroot}
 %config(noreplace) %{_sysconfdir}/%{name}/chromium.profile
 %config(noreplace) %{_sysconfdir}/%{name}/chromium-browser.profile
 %config(noreplace) %{_sysconfdir}/%{name}/login.users
-%{_bindir}/*
-%{_docdir}/*
-%{_mandir}/*
+/usr/bin/firejail
+/usr/bin/firemon
+/usr/lib/firejail/libtrace.so
+/usr/share/doc/packages/firejail/COPYING
+/usr/share/doc/packages/firejail/README
+/usr/share/doc/packages/firejail/RELNOTES
+/usr/share/man/man1/firejail.1.gz
+/usr/share/man/man1/firemon.1.gz
 /usr/share/bash-completion/completions/firejail
  
 %post
 chmod u+s /usr/bin/firejail
 
 %changelog
+* Wed Oct 8 2014  netblue30 <netblue30@yahoo.com> 0.9.14-1
+ - Added support for user defined seccomp blacklists
+ - Added syscall trace support
+ - Added --tmpfs option
+ - Added --balcklist option
+ - Added --read-only option
+ - Added --bind option
+ - Logging enhancements
+ - --overlay option was reactivated
+ - Added firemon support to print the ARP table for each sandbox
+ - Added firemon support to print the route table for each sandbox
+ - Added firemon support to print interface information for each sandbox
+ - bugfixes
+
 * Tue Sep 16 2014 netblue30 <netblue30@yahoo.com> 0.9.12-1
  - Added capabilities support
  - Added support for CentOS 7
@@ -103,6 +126,7 @@ chmod u+s /usr/bin/firejail
 
 EOF
 
+echo "building rpm"
 rpmbuild -ba SPECS/firejail.spec
 rpm -qpl RPMS/x86_64/firejail-$VERSION-1.x86_64.rpm
 cd ..

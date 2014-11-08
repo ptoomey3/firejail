b92449dc6f77fe4a704b291c2bb4161932dbad61
netblue30 <netblue30@a7c41c1e-9cec-41a6-8168-6dbd64aeb3cc>
2014-11-08 17:10:51 +0000

    testing

--- test/auto/autotest.sh
+++ test/auto/autotest.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
 # remove previous reports and output file
-rm -f output*
-rm -f report*
-rm -f outrep*
+cleanup() {
+	rm -f output*
+	rm -f report*
+	rm -f outrep*
+	rm -fr firejail-trunk
+}
+
+while [ $# -gt 0 ]; do    # Until you run out of parameters . . .
+    case "$1" in
+    --cleanup)
+    	cleanup
+    	exit
+	;;
+    --help)
+    	echo "./autotest.sh [--cleanup|--help]"
+    	exit
+    	;;
+    esac
+    shift       # Check next set of parameters.
+done
+
+
+cleanup
 # enable sudo
 sudo ls -al
 
@@ -14,7 +34,6 @@ sudo ls -al
 # - check compilation,
 # - install
 #*****************************************************************
-rm -fr firejail-trunk
 svn checkout svn://svn.code.sf.net/p/firejail/code-0/trunk firejail-trunk
 cd firejail-trunk
 ./configure --prefix=/usr 2>&1 | tee ../output-configure
@@ -33,6 +52,28 @@ cat report-test1
 echo
 echo
 echo
+
+#*****************************************************************
+# TEST 1.1
+#*****************************************************************
+# - run cppcheck
+#*****************************************************************
+cd firejail-trunk
+cp /home/netblue/bin/cfg/std.cfg .
+cppcheck --force . 2>&1 | tee ../output-cppcheck
+cd ..
+grep error output-cppcheck > report-test1.1
+echo
+echo
+echo
+echo "TEST 1.1 Report:"
+cat report-test1.1
+echo
+echo
+echo
+
+
+
 
 
 #*****************************************************************
@@ -55,6 +96,30 @@ cd firejail-trunk/test
 cd ../..
 grep TESTING output-test3 > ./report-test3
 
+
+
+#*****************************************************************
+# TEST 4
+#*****************************************************************
+# - expect test as root, no malloc perturb
+#*****************************************************************
+export MALLOC_CHECK_=3
+export MALLOC_PERTURB_=$(($RANDOM % 255 + 1))
+cd firejail-trunk/test
+sudo ./test-root.sh 2>&1 | tee ../../output-test4 | grep TESTING
+cd ../..
+grep TESTING output-test4 > ./report-test4
+
+#*****************************************************************
+# TEST 5
+#*****************************************************************
+# - expect test as user, no malloc perturb
+#*****************************************************************
+cd firejail-trunk/test
+./test.sh 2>&1 | tee ../../output-test5 | grep TESTING
+cd ../..
+grep TESTING output-test5 > ./report-test5
+
 #*****************************************************************
 # PRINT REPORTS
 #*****************************************************************
@@ -66,15 +131,24 @@ echo
 echo
 echo "TEST 1 Report:"
 cat report-test1
+echo "TEST 1.1 Report:"
+cat report-test1.1
 echo "TEST 2 Report:"
 cat ./report-test2 
 echo "TEST 3 Report:"
 cat ./report-test3 
+echo "TEST 4 Report:"
+cat ./report-test4 
+echo "TEST 5 Report:"
+cat ./report-test5 
 echo
 
 cat report-test1 > outrep1
+cat report-test1.1 > outrep1.1
 grep ERROR report-test2 > outrep2
 grep ERROR report-test3 > outrep3
+grep ERROR report-test4 > outrep4
+grep ERROR report-test5 > outrep5
 wc -l outrep*
 echo
 
